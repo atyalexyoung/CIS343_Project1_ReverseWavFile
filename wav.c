e6b4b545c92a3b7a650041503c33e65d3c3090e2
atyalexyoung <atyalexyoung@gmail.com>
2023-09-06 19:34:27 -0400

    changes

--- wav.c
+++ wav.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+#include "wav.h"
+
+
+
+
+
+
+int load_file(char* data){
+    size_t size;
+    char* file_contents = read_file("Abdication.wav", &size);
+    struct wav_file audio;
+    audio.wav_header_pointer = file_contents;
+    audio.file_size = size;
+    audio.data = file_contents[44];
+
+    return;
+}
--- wav.h
+++ wav.h
@@ -3,10 +3,11 @@
 
 /***
  * holds information about the header of the wav file
+ * add information as needed
 */
 struct wav_header
 {
-
+    
 };
 
 
@@ -15,22 +16,33 @@ struct wav_header
 */
 struct wav_file
 {
-    struct wav_header* wave_header_pointer;
+    struct wav_header* wav_header_pointer;
     int file_size;
     char* data;
 };
 
 /***
  * Gets the header information from the data
+ * gets first 44 bytes of file, extracts data, creates header struct
 */
-int get_header(char* data, char* header_location);
+int get_header(char* data, char* wav_file);
 
 
-/***
- * gets 
+/**
+ * gets a file from disk into memory
+ * calls read_file function from file library
+ * takes retruned bytes and creates new wav_file
+ * then set the header, data size, and pointer to data
 */
 int load_file(char* data);
 
+
+/***
+ * gets the file from disc and loads into memory
+ * takes wav_file and file path, prepares a byte array of the file
+ * then calls write_file from file lib to write to disk
+ * 
+*/
 int save_file(struct wav_file, char* file_path);
 
 
