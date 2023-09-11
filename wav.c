ad142f61b43f44cdf59b945f3d6dd7565d1ad498
mppeters <89791006+mppeters@users.noreply.github.com>
2023-09-11 14:30:46 -0400

    Update wav.c
    changed get header func

--- wav.c
+++ wav.c
@@ -41,54 +41,46 @@ int save_file(struct wav_file audio, char* file_path)
 */
 
 //pass in pointer to the wav_file struct... no need to pass in wav_header nbecause we havea  wav_header in the wav_file
-int get_header(struct wav_file audio, struct wav_header header)
+int get_header(struct wav_file* audio)
 {
     int riff_status, file_type_status, format_chunk_status, data_header_status;
 
     // get riff and check if riff
-    header.riffy_boi = audio.wav_header_pointer;
-    riff_status = check_riff(header.riffy_boi);
+    
+    riff_status = check_riff(&audio->wav_header_pointer[0]);
     if(riff_status != 0)
     {
         return 1;
     }
 
     // get file type and check if 'WAVE'
-    header.file_type = &audio.wav_header_pointer[8];
-    file_type_status = check_file_type(header.file_type);
+    
+    file_type_status = check_file_type(&audio->wav_header_pointer[8]);
     if(file_type_status != 0)
     {
         return 1;
     }
 
     // get format chunk and check if 'fmt'
-    header.format_chunk = &audio.wav_header_pointer[12];
-    format_chunk_status = check_format_chunk_status(header.format_chunk);
+    
+    format_chunk_status = check_format_chunk_status(&audio->wav_header_pointer[12]);
     if(format_chunk_status != 0)
     {
         return 1;
     }
 
     // get data header and check if 'data' 36-39
-    header.data_header = &audio.wav_header_pointer[36];
-    data_header_status = check_header_status(header.data_header);
+    
+    data_header_status = check_header_status(&audio->wav_header_pointer[36]);
     if(data_header_status != 0)
     {
         return 1;
     }    
 
     // NEED TO BE PRINTED OUT
-    header.num_channels = &audio.wav_header_pointer[22]; // 22-23
-    header.sample_rate = &audio.wav_header_pointer[24]; // 24-27
+    audio->wav_header_pointer->num_channels = &audio->wav_header_pointer[22]; // 22-23
+    audio->wav_header_pointer->sample_rate = &audio->wav_header_pointer[24];// 24-27
     //********************************************
-
-
-    // useless??? not as useless as me
-    header.num_bytes = &audio.wav_header_pointer[4]; // number of bytes after subracting 8 4-7
-    header.length_of_format_data = &audio.wav_header_pointer[16]; //16-19
-    header.format_type = &audio.wav_header_pointer[20]; // 20-21
-    header.bits_per_sample = &audio.wav_header_pointer[34]; // 34-35
-    header.data_section_size = &audio.wav_header_pointer[34]; // 40-43
 
     return 0;
 }
